22462a36b9fd86f68b162b9c5f10f4a9458e1a4f
njwmerv <montoya.nicanor04@gmail.com>
2026-05-18 23:08:09 -0400

    removed unused helper

--- parser/parser.h
+++ parser/parser.h
@@ -7,6 +7,4 @@
 
 std::unique_ptr<ASTNode> parse(const std::vector<Token>& stream, std::ostream& err);
 
-void print_AST(const std::unique_ptr<ASTNode>& root, size_t depth, std::ostream& os);
-
 #endif //PARSER_H
